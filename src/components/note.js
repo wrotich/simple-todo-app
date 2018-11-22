27136442372f18b4cd9fca3bc1ed662183c577c9
wrotich <winniecherop@outlook.com>
2018-11-22 12:11:49 +0300

    Fix failing build

--- src/components/note.js
+++ src/components/note.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Note from './components/note'
-import './App.css';
 
 class Note extends Component {
 
   render() {
     return (
-      <div className="note" onClick={this.props.deleteMethod}>
+      <div className="note" onClick={this.props.deleteMethod}> 
       {this.props.text}
         
       </div>
